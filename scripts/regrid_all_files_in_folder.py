0865e7ba9a4a3ca2a3f5df3cce97c0fe89466bad
Mariana Vertenstein <marianav@met.no>
2026-01-08 12:17:13 +0100

    added new argparse capability

--- scripts/regrid_all_files_in_folder.py
+++ scripts/regrid_all_files_in_folder.py
@@ -1,67 +1,118 @@
-#!/bin/env python
+#!/usr/bin/env python3
+
+"""
+script: regrid_all_files_in_folder
+regrids all cam or clm output files from spectral element grid to output lat/lon grid 
+"""
+
+#++++++++++++++++++++++++++++++
+# Import python modules
+#++++++++++++++++++++++++++++++
 
 import os
 import sys
 import glob
-
-import numpy as np
+import argparse
 import xarray as xr
 
-sys.path.append(os.path.join(os.path.dirname(__file__), "../", "src"))
+# Determine local directory path:
+_LOCAL_PATH = os.path.dirname(os.path.abspath(__file__))
 
+# Append path to regridding utilities
+sys.path.append(os.path.join(_LOCAL_PATH, "../", "src"))
+
+# Now import regridding utilities
 from noresm_pyregridding import noresm_pyregridding
 
-def print_help_message():
-    print("Usage: python regrid_se_data.py <run_path>")
-    print("<run_path> : Path to se output data, path to folder is expected!")
-    sys.exit(1)
+#++++++++++++++++++++++++++++++
+# Input argument parser function
+#++++++++++++++++++++++++++++++
 
-# Making sure there is a run_path argument
-if len(sys.argv) < 2:
-    print("You must supply a path to land output data, path to lnd/hist folder is expected!")
-    print_help_message()
-if sys.argv[1] == "--help":
-    print_help_message()
-run_path = sys.argv[1]
-print (f"run_path is {run_path}")
-if not os.path.exists(run_path):
-    print("You must supply a path to land output data,  path to lnd/hist folder is expected!")
-    print(f"path {run_path} does not exist")
-    print_help_message()
+def parse_arguments():
 
-weight = "/datalake/NS9560K/diagnostics/land_xesmf_diag_data/map_ne16pg3_to_1.9x2.5_nomask_scripgrids_c250425.nc"
+    """
+    Parses command-line input arguments using the argparse
+    python module and outputs the final argument object.
+    """
 
-# TODO: determine mapping type (conservative versus bilinear)
+    #Create parser object:
+    parser = argparse.ArgumentParser(description='Command-line wrapper to regridder from SE to lat/lon grid')
 
-# Determine dimension name (currently lndgrid for clm and ncols for atm)
-#dimname = 'ncols'
-dimname = 'lndgrid'
+    parser.add_argument('--debug', action='store_true',
+                        help="Turn on debug output.")
+
+    parser.add_argument('--indir',
+                        help="Full path to directory containing input spectral element data files (required)",
+                        required=True)
+
+    parser.add_argument('--outdir',
+                        help="Full path to directory where output regridded data will be placed (required)",
+                        required=True)
+
+    parser.add_argument ('--indim',
+                         choices=("lndgrid","ncol"),
+                         help="dimension name to regrid on input files (required)",
+                         required=True)
+
+    parser.add_argument ('--ingrid',
+                         choices=("ne16","ne30"),
+                         help="input_grid name (required)",
+                         required=True)
+
+    # Parse Argument inputs
+    args = parser.parse_args()
+
+    # Error checks
+    if not os.path.exists(args.indir):
+        errmsg = f"ERROR: input pathname {args.indir} does not exist"
+        parser.error(errmsg)
+
+    input_files = glob.glob(f"{args.indir}*.nc")
+    if len(input_files) < 1:
+        parser.error(f"No netcdf files found in {args.indir}")
+
+    return args
+
+#++++++++++++++++++++++++++++++
+# main regridding script
+#++++++++++++++++++++++++++++++
+
+# Parse command-line arguments
+args = parse_arguments()
+
+# TODO: the following is only for area average - need to introduce more generality for bilinear mapping for
+# intensive variables 
+
+# Determine weights file to use for regridding
+if (args.ingrid == 'ne16'):
+    weight_file = "/datalake/NS9560K/diagnostics/land_xesmf_diag_data/map_ne16pg3_to_1.9x2.5_nomask_scripgrids_c250425.nc"
+elif (args.ingrid == 'ne30'): 
+    weight_file = "/datalake/NS9560K/diagnostics/land_xesmf_diag_data/map_ne30pg3_to_0.5x0.5_nomask_aave_da_c180515.nc"
+else:
+    raise Exception("only input grids of ne16 and ne30 are currently supported")
 
 # Create regridder
 print (f"Creating regridder for conservative maping")
-regridder = noresm_pyregridding.make_se_regridder(weight_file=weight)
+regridder = noresm_pyregridding.make_se_regridder(weight_file=weight_file)
 print (f"successfully called regridder")
-outpath = "out_dump"
-
-# Determine list of files to regrid
-filelist = glob.glob(f"{run_path}*.nc")
-print (f"filelist is {filelist}")
-if len(filelist) < 1:
-    print(f"No netcdf files found in {run_path}")
-    print_help_message()
 
 # For each file in list of files - regridd data
+input_dir = args.indir
+output_dir = args.outdir
+filelist = glob.glob(f"{input_dir}*.nc")
 for filepath in filelist:
 
     # Regrid file
     print(f"Regridding file {filepath}")
     data = xr.open_dataset(filepath)
+    dimname = args.indim
+    print (f" using dimname of {dimname}")
     data_regridded = noresm_pyregridding.regrid_se_data(regridder, data, dimname)
     print(f"Successfully regridded file {filepath}")
 
     # Write  out regridded file
     filename = filepath.split("/")[-1]
-    output_file = os.path.join(outpath, filename.replace(".nc", "_regridded.nc"))
+    output_file = os.path.join(output_dir, filename.replace(".nc", "_regridded.nc"))
     data_regridded.to_netcdf(output_file)
     print(f"Wrote regridded file {output_file}")
     sys.exit(4)
