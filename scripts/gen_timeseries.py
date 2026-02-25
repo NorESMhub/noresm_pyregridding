31a120b4c7496c7e649c60319c9577f93adac267
Mariana Vertenstein <marianav@met.no>
2026-02-25 12:39:06 +0100

    ../../scripts/parallel_convert2NetCDF4Classic.sh

--- scripts/gen_timeseries.py
+++ scripts/gen_timeseries.py
@@ -50,21 +50,33 @@ def parse_arguments():
 
     parser.add_argument('--inputdir', type=str,
                         help="Comma separated full pathnames of directories containing input spectral element data files (required)",
-                        required=True)
-
+                        required=True
+                        )
     parser.add_argument("--realm",
-                        choices=["atm","lnd"],
+                        choices=["atmos","land"],
                         help="Realm to process - sets include patterns for time series (required)",
-                        required=True,)
-
+                        required=True
+                        )
     parser.add_argument('--outputdir', type=str,
-                        help="Full path to directory where output regridded data will be placed (required)",
-                        required=True)
-
+                        help="Full path to directory where output time series data will be placed (optional) \n"
+                        "if not specified will be put in inputdir/../time_series)",
+                        )
     parser.add_argument("--overwrite_timeseries",
                         action="store_true",
                         help="Overwrite existing timeseries outputs (default: False)",
-    )
+                        )
+    parser.add_argument("--year_first",
+                        type=int,
+                        help="first year of history files to use",
+                        ),
+    parser.add_argument("--year_last",
+                        type=int,
+                        help="last year of history files to use",
+                        ),
+    parser.add_argument("--year_inc",
+                        type=int,
+                        help="how many years to use for each time series file",
+                        ),
     parser.add_argument("--workers",
                         type=int,
                         default=1,
@@ -111,18 +123,21 @@ def main():
     debug = args.debug
 
     # Determine include patterns
-    if args.realm == "atm":
+    if args.realm == "atmos":
         include_patterns = ["*cam.h0a*"]
         frequency = "mon"
-    elif args.realm == "lnd":
-        include_patterns = ["*clm2.h0*"]
+    elif args.realm == "land":
+        include_patterns = ["*clm2.h0a*"]
         frequency = "mon"
 
     # Determine input directory
     inputdir = Path(args.inputdir)
 
     # Determine output directories
-    outputdir = Path(args.outputdir)
+    if args.outputdir:
+        outputdir = Path(args.outputdir)
+    else:
+        outputdir = inputdir / '..' / 'time_series'
 
     # Create time series by default
     logger.info(f"Timeseries generation starting for files in {inputdir}...")
@@ -136,32 +151,44 @@ def main():
         logger.warning(f"No input files to process in {inputdir} with {include_patterns}")
         sys.exit(0)
 
+    year_first = args.year_first
+    year_last = args.year_last
+    nstep = args.year_inc
+
     # Create base HFCollection
     hf_collection = HFCollection(inputdir, dask_client=client)
 
     for include_pattern in include_patterns:
         logger.info("Processing files with pattern: %s", include_pattern)
 
-        # Create a filtered collection containing only files matching the current pattern
-        hfp_collection = hf_collection.include_patterns([include_pattern])
+        for year in range(year_first, year_last+1, nstep):
+            logger.info(f"Processing from year {year} to year {year+nstep-1}")
+            hfp_collection = hf_collection.include_patterns([include_pattern])
+            hfp_collection = hfp_collection.include_years(year, year+nstep-1)
 
-        # Reads metadata from all files matching this pattern
-        # Gets variable names, dimensions, time information, etc.
-        hfp_collection.pull_metadata()
+            logger.info(f"files to process for year {year} are") 
+            for item in list(hfp_collection):
+                logger.info(f"{item}")
 
-        # Set up the time series generation for this pattern's files
-        ts_collection = TSCollection(
-            hfp_collection, outputdir, ts_orders=None, dask_client=client
-        )
+            # Reads metadata from all files matching this pattern
+            # Gets variable names, dimensions, time information, etc.
+            hfp_collection.pull_metadata()
 
-        # Apply overwrite if requested:
-        # If --overwrite flag was passed, tells GenTS to overwrite existing time series files
-        if args.overwrite_timeseries:
-            ts_collection = ts_collection.apply_overwrite("*")
+            # Set up the time series generation for this pattern's files
+            logger.info("Calling ts_collection")
+            ts_collection = TSCollection(
+                hfp_collection, outputdir, ts_orders=None, dask_client=client
+            )
+            logger.info("Finished ts_collection")
 
-        # Perform the time series generation for this pattern
-        ts_collection.execute()
-        logger.info("Timeseries processing complete")
+            # Apply overwrite if requested:
+            # If --overwrite flag was passed, tells GenTS to overwrite existing time series files
+            if args.overwrite_timeseries:
+                ts_collection = ts_collection.apply_overwrite("*")
+
+            # Perform the time series generation for this pattern
+            ts_collection.execute()
+            logger.info("Timeseries processing complete")
 
     if client:
         client.close()
